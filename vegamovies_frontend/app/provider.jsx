acb70363e469d06f9575ca736eb0c476fb641341
Sahil <113676978+whonix2904@users.noreply.github.com>
2024-06-19 21:35:31 +0530

    commit

--- vegamovies_frontend/app/provider.jsx
+++ vegamovies_frontend/app/provider.jsx
@@ -10,9 +10,17 @@ export const Provider = ({ children }) => {
             style={{ boxShadow: "rgba(0, 0, 0, 0.3) 0px 0px 20px 0px" }}
             className=" bg-[#191919]"
           >
+            <section>
             <Header />
+            </section>
+            <section>
             <Navigation />
-            <div>{children}</div>
+            </section>
+            <section>{children}</section>
+            <section className="flex flex-col text-center mt-10 gap-8 mx-10">
+            <h1 className="text-2xl"><em>Vegamovies4u.cc, vegamovies.mex.com, vegamovies.vg, Vegamovies nl, Vegamovies Netflix, Vegamovies in, Vegamovies.com, Vegamovies Hdhub4u, Vegamovies 2.0, Vegamovies.</em></h1>
+            <p className="text-lg"><strong className="text-red-600">Disclaimer:</strong> Our website does not host any files on its servers. All files available for download on Vegamovies are hosted by third-party servers and provided by external sources over which we have no control. We do not endorse or promote any specific content available for download.</p>
+            </section>
           </div>
         </div>
       </main>
